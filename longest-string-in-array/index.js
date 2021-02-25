b2e2527772d37f43d09a0ddde16bc669e7e06f20
tons <anthony_dejesus@worldvision.org.ph>
2021-02-25 21:24:09 +0800

    first commit

---
+++ longest-string-in-array/index.js
@@ -0,0 +1,18 @@
+const getLongestStringFromArray = (array, currentLongest = "") => {
+    // if this is the last item in the array compare the value to the last longest string value
+    if (array.length === 1) {
+        return array[0].length > currentLongest.length ? array[0] : currentLongest
+    }
+ 
+    // get the first value from array
+    const currentValue = array.shift();
+
+    // remove first value from array
+    const newArray = array.filter(arr => {
+        return arr !== currentValue
+    })
+
+    return currentLongest.length < currentValue.length ? getLongestStringFromArray(newArray, currentValue) : getLongestStringFromArray(newArray, currentLongest)
+} 
+
+console.log(getLongestStringFromArray(['abbbbbb', 'b', 'abc', 'bbb', 'ab', 'abac']));
