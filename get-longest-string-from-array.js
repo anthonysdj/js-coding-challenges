3e8d8056a82603d2e022f211f2df01eca5aab3ed
tons <anthony_dejesus@worldvision.org.ph>
2021-02-28 12:11:00 +0800

    use slice for removing first val in arr

--- get-longest-string-from-array.js
+++ get-longest-string-from-array.js
@@ -1,5 +1,5 @@
 const getLongestStringFromArray = (array, currentLongest = "") => {
-    if (!Array.isArray(array) || array.length < 1) return 'Input must be a non-empty array';
+    if (!array || !Array.isArray(array) || array.length < 1) return null;
 
     // if this is the last item in the array compare the value to the last longest string value
     if (array.length === 1) {
@@ -7,12 +7,10 @@ const getLongestStringFromArray = (array, currentLongest = "") => {
     }
  
     // get the first value from array
-    const currentValue = array.shift();
+    const currentValue = array.concat().shift();
 
     // remove first value from array
-    const newArray = array.filter(arr => {
-        return arr !== currentValue
-    })
+    const newArray = array.slice(1)
 
     return currentLongest.length < currentValue.length ? getLongestStringFromArray(newArray, currentValue) : getLongestStringFromArray(newArray, currentLongest)
 }
--- get-longest-string-from-array.test.js
+++ get-longest-string-from-array.test.js
@@ -1,7 +1,20 @@
 const { test, expect } = require("@jest/globals");
-const { keyword } = require("chalk");
 
 const getLongestStringFromArray = require('./get-longest-string-from-array')
+
+test('should return null if no input provided', () => {
+    const longestString = getLongestStringFromArray()
+
+    expect(longestString).toBe(null)
+})
+
+test('should return null if non array input is provided', () => {
+    const longestStringInt = getLongestStringFromArray(123)
+    const longestStringStr = getLongestStringFromArray('string')
+
+    expect(longestStringInt).toBe(null)
+    expect(longestStringStr).toBe(null)
+})
 
 test('should return the longest string from a string array', () => {
     const longestString = getLongestStringFromArray(['aa', 'bbbb', 'ccc'])
@@ -10,7 +23,7 @@ test('should return the longest string from a string array', () => {
 })
 
 test('should return the first longest string if longest string are more than 1', () => {
-    const longestString = getLongestStringFromArray(['aa', 'ddd', 'ccc'])
+    const longestString = getLongestStringFromArray(['aa', 'ddd', 'cccccc'])
 
-    expect(longestString).toBe('ddd')
+    expect(longestString).toBe('cccccc')
 })
